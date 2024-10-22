36bfbb742994ae1a4a39fac18716d6a53779fb53
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2024-10-22 13:54:17 +0530

    Initial commit

--- backend/shopping/src/main/java/com/teamx/shopping/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/teamx/shopping/repository/ProductRepository.java
-package com.teamx.shopping.repository;
-
-import com.teamx.shopping.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    boolean existsByName(String name);
-}
